54a0f01855549d5584ec533f182db0c381d77eac
Yansokchan <prochnjr2299@gmail.com>
2025-12-23 09:02:54 +0700

    Enhance animation of splashscreen

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -5,6 +5,7 @@ import { ShootingStars } from "./ui/shooting-stars";
 import { CanvasRevealEffect } from "./ui/CanvasRevealEffect";
 import Astronaut from "./ui/Astronaut";
 import PlayButton from "./ui/PlayButton";
+import { SplittingText } from "./ui/shadcn-io/splitting-text";
 
 interface SplashScreenProps {
   onComplete: () => void;
@@ -36,7 +37,7 @@ const SplashScreen = ({ onComplete }: SplashScreenProps) => {
 
   const moonDelay = 0;
   const astronautDelay = 2;
-  const buttonDelay = 2;
+  const buttonDelay = 4.2;
 
   const slideLeftVariant = {
     hidden: { x: -100, opacity: 0 },
@@ -55,8 +56,42 @@ const SplashScreen = ({ onComplete }: SplashScreenProps) => {
       transition: { duration: 0.5, delay: 0 }, // Text exits after moon starts going up
     },
   };
+  const slideRightVariant = {
+    hidden: { x: 100, opacity: 0 },
+    visible: (i: number) => ({
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: i * 0.2 + 1,
+        duration: 0.9,
+        ease: "easeOut",
+      },
+    }),
+    exit: {
+      x: 100,
+      opacity: 0,
+      transition: { duration: 0.5, delay: 0 }, // Text exits after moon starts going up
+    },
+  };
 
   const slideUpVariant = {
+    hidden: { y: 60, opacity: 0 },
+    visible: (i: number) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: i * 0.2 + 1.5,
+        duration: 0.8,
+        ease: "easeOut",
+      },
+    }),
+    exit: {
+      y: 60,
+      opacity: 0,
+      transition: { duration: 0.5, delay: 0 }, // Text exits after moon starts going up
+    },
+  };
+  const slideDownVariant = {
     hidden: { y: 60, opacity: 0 },
     visible: (i: number) => ({
       y: 0,
@@ -120,7 +155,7 @@ const SplashScreen = ({ onComplete }: SplashScreenProps) => {
           <motion.div
             initial={{ opacity: 0, scale: 0.95 }}
             animate={{ opacity: 1, scale: 1 }}
-            className="relative z-10 flex flex-col items-center justify-center gap-8"
+            className="relative z-10 flex flex-col items-center justify-center"
           >
             <motion.div className="absolute left-1/2 top-1/2 h-96 w-96 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#4deeea]/15 blur-3xl" />
             <motion.div className="absolute left-1/4 top-1/3 h-80 w-80 -translate-x-1/2 -translate-y-1/2 rounded-full bg-fuchsia-500/10 blur-3xl mix-blend-screen" />
@@ -129,56 +164,90 @@ const SplashScreen = ({ onComplete }: SplashScreenProps) => {
             {/* Text Content */}
             <div className="flex flex-col z-10 items-center justify-center gap-2 mt-20">
               {/* Line 1: Welcome To My */}
-              <div className="flex flex-wrap justify-center gap-x-3 gap-y-1 text-3xl md:text-5xl font-bold text-white tracking-tight">
-                {["Welcome", "To", "My"].map((word, i) => (
-                  <motion.span
-                    key={word}
-                    custom={i}
-                    variants={slideLeftVariant}
-                    initial="hidden"
-                    animate={isExiting ? "exit" : "visible"}
-                  >
-                    {word}
-                  </motion.span>
-                ))}
-              </div>
-
-              {/* Line 2: Portfolio Website */}
-              <div className="flex flex-wrap justify-center gap-x-3 gap-y-1 text-3xl md:text-5xl font-bold">
-                {["Space"].map((word, i) => (
-                  <motion.span
-                    key={word}
-                    custom={i}
-                    variants={slideUpVariant}
-                    initial="hidden"
-                    animate={isExiting ? "exit" : "visible"}
-                    className="bg-clip-text text-gradient"
-                  >
-                    {word}
-                  </motion.span>
-                ))}
+              <div className="flex flex-col gap-2 justify-center text-center gap-y-1 text-3xl md:text-5xl font-bold text-white tracking-tight">
+                <motion.span
+                  variants={slideLeftVariant}
+                  initial="hidden"
+                  animate={isExiting ? "exit" : "visible"}
+                >
+                  <SplittingText
+                    text="WELCOME TO MY"
+                    type="chars"
+                    delay={400}
+                  />
+                </motion.span>
+                <motion.span
+                  variants={slideRightVariant}
+                  animate={isExiting ? "exit" : "visible"}
+                >
+                  <SplittingText text="SPACE" type="chars" delay={1500} />
+                </motion.span>
               </div>
             </div>
 
             {/* Play Button Section */}
+            <div className="flex flex-col items-center justify-center gap-2 mt-12">
+              <motion.span
+                variants={slideLeftVariant}
+                initial="hidden"
+                animate={isExiting ? "exit" : "visible"}
+              >
+                <SplittingText
+                  className="text-sm text-white/80"
+                  type="words"
+                  inView
+                  delay={2000}
+                  text="Thank you for visiting my portfolio website!"
+                  motionVariants={{
+                    initial: {
+                      y: 50,
+                      scale: 0.5,
+                      opacity: 0,
+                      x: 50,
+                      rotate: 90,
+                    },
+                    animate: { y: 0, scale: 1, opacity: 1, x: 0, rotate: 0 },
+                    transition: { duration: 0.5, ease: "easeOut" },
+                  }}
+                />
+              </motion.span>
+              <motion.span
+                variants={slideRightVariant}
+                initial="hidden"
+                animate={isExiting ? "exit" : "visible"}
+              >
+                <SplittingText
+                  className="text-sm text-white/80"
+                  type="chars"
+                  inView
+                  delay={3000}
+                  text="Are you ready to see me?"
+                  motionVariants={{
+                    initial: {
+                      y: 50,
+                      scale: 0.5,
+                      opacity: 0,
+                      x: 50,
+                      rotate: 90,
+                    },
+                    animate: { y: 0, scale: 1, opacity: 1, x: 0, rotate: 0 },
+                    transition: { duration: 0.5, ease: "easeOut" },
+                  }}
+                />
+              </motion.span>
+            </div>
+
             <motion.div
-              initial={{ opacity: 0, y: 30 }}
+              initial={{ scale: 0 }}
               animate={{
-                opacity: isExiting ? 0 : 1,
-                y: isExiting ? 30 : 0,
+                scale: isExiting ? 0 : 1,
               }}
               transition={{
                 delay: isExiting ? 0 : buttonDelay, // Exit immediately
-                duration: 1,
+                duration: 0.5,
               }}
-              className="flex flex-col items-center justify-center gap-4 mt-12"
+              className="flex flex-col items-center justify-center gap-4 mt-5"
             >
-              <p className="text-white/80 text-sm text-center font-light tracking-wide">
-                Thank you for visiting my portfolio website!
-              </p>
-              <p className="text-white/80 text-sm text-center font-light tracking-wide">
-                Are you ready to see me?
-              </p>
               <div className="relative z-40">
                 <PlayButton onClick={() => setIsExiting(true)} />
               </div>
